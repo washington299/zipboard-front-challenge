da6500c8728fdadd24ad6ebb72b5b80d52187999
Washington Campos <washingtoncampos9@gmail.com>
2021-09-01 15:09:43 -0300

    feat: create ButtonDefaultStyles global element

--- imports/ui/styles/globalElements.js
+++ imports/ui/styles/globalElements.js
@@ -1,6 +1,6 @@
 import { css } from "styled-components";
 
-export const InputStyles = css`
+export const InputDefaultStyles = css`
 	font-size: var(--small);
 	border: 1px solid var(--gray);
 	border-radius: var(--extra-small);
@@ -9,4 +9,13 @@ export const InputStyles = css`
 	&::placeholder {
 		color: var(--dark-gray);
 	}
+`;
+
+export const ButtonDefaultStyles = css`
+	width: 100%;
+	border: none;
+	border-radius: var(--extra-small);
+	font-weight: var(--font-bold);
+	outline: none;
+	cursor: pointer;
 `;
