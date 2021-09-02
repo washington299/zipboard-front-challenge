fd28698c54ce65f71bd84d354728c68e76a1b2cc
Washington Campos <washingtoncampos9@gmail.com>
2021-09-02 17:18:30 -0300

    feat: create users method to insert new user

---
+++ imports/api/usersMethods.js
@@ -0,0 +1,16 @@
+import { Meteor } from "meteor/meteor";
+import { Accounts } from "meteor/accounts-base";
+import { check } from "meteor/check";
+
+Meteor.methods({
+	"users.insert"({ username, email, password }) {
+		check(username, String);
+		check(email, String);
+		check(password, String);
+
+		if (Accounts.findUserByUsername(username)) return "Username already exists";
+		if (Accounts.findUserByEmail(email)) return "E-mail already exists";
+
+		Accounts.createUser({ email, password, createdAt: new Date() });
+	},
+});
--- server/main.js
+++ server/main.js
@@ -1,11 +1,1 @@
-import { Meteor } from "meteor/meteor";
-import { Accounts } from "meteor/accounts-base";
-
-const SEED_EMAIL = "washingtoncampos9@gmail.com";
-const SEED_PASSWORD = "123";
-
-Meteor.startup(() => {
-  if (!Accounts.findUserByEmail(SEED_EMAIL)) {
-    Accounts.createUser({ email: SEED_EMAIL, password: SEED_PASSWORD });
-  }
-});
+import "/imports/api/usersMethods";
