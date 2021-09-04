44aa1ebe4799c5f2be81a4ce697f13079631e5fe
Washington Campos <washingtoncampos9@gmail.com>
2021-09-04 20:20:39 -0300

    feat: add default tests

--- tests/main.js
+++ tests/main.js
@@ -1,20 +1,20 @@
 import assert from "assert";
 
 describe("zipboard", function () {
-  it("package.json has correct name", async function () {
-    const { name } = await import("../package.json");
-    assert.strictEqual(name, "zipboard");
-  });
+	it("package.json has correct name", async function () {
+		const { name } = await import("../package.json");
+		assert.strictEqual(name, "zipboard");
+	});
 
-  if (Meteor.isClient) {
-    it("client is not server", function () {
-      assert.strictEqual(Meteor.isServer, false);
-    });
-  }
+	if (Meteor.isClient) {
+		it("client is not server", function () {
+			assert.strictEqual(Meteor.isServer, false);
+		});
+	}
 
-  if (Meteor.isServer) {
-    it("server is not client", function () {
-      assert.strictEqual(Meteor.isClient, false);
-    });
-  }
+	if (Meteor.isServer) {
+		it("server is not client", function () {
+			assert.strictEqual(Meteor.isClient, false);
+		});
+	}
 });
